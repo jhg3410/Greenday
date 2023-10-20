48a242ff509ba65d04903b80618ae51b7c194978
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2023-10-20 21:11:24 +0900

    statusBar color 변경

--- core/designsystem/src/main/java/watcha/test/greenday/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/watcha/test/greenday/core/designsystem/theme/Theme.kt
@@ -92,8 +92,9 @@ fun GreendayTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars =
+                darkTheme.not()
         }
     }
 
